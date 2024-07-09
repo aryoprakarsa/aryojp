bade768e8812d3563c66cddeb7b5dd61bace96dc
Aryo Prakarsa <30886148+aryoprakarsa@users.noreply.github.com>
2024-07-10 02:11:02 +0700

    Removed image and updated under construction page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import constructionImage from './construction.png'; // add your construction image to src folder
 
 function App() {
   return (
@@ -8,7 +7,6 @@ function App() {
       <header className="App-header">
         <h1>We're Under Construction</h1>
         <p>Our site is currently under construction. Please check back soon!</p>
-        <img src={constructionImage} alt="Under Construction" className="construction-image" />
       </header>
     </div>
   );
