d74e45442b4cac6937d77e167fb999402c6d122f
Aryo Prakarsa <30886148+aryoprakarsa@users.noreply.github.com>
2024-07-14 23:56:06 +0700

    fix css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>We're Under Construction</h1>
-        <p>Our site is currently under construction. Please check back soon!</p>
+        <h1>Under Construction</h1>
+        <p>
+          My site is currently under construction. Please check back soon! In
+          the meantime, interested in cool stuff? Check out my{" "}
+          <a className="external-link" href="https://speedtest.aryo.ai/">
+            speedtest
+          </a>
+          .
+        </p>
       </header>
     </div>
   );
